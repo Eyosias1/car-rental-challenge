9d4858c4181b196cca8b179f9059fec075a837fb
Eyosias Woldemichael <eyosias16@gmail.com>
2024-05-13 17:43:22 +0200

    commit me

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,7 @@
 # exit on error
 set -o errexit
 
-brew install postgresql@15 libpq
-brew link --force libpq
-brew services start postgresql@15
-bundle install
+
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
